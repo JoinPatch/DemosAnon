3dd39371be06bb2681aa9b36dca24e05cc2054bf
timf34 <farrelti@tcd.ie>
2025-06-22 16:54:48 +0100

    UPDATE

--- src/components/ASCIIHeader.jsx
+++ src/components/ASCIIHeader.jsx
@@ -1,25 +1,20 @@
 // src/components/ASCIIHeader.jsx
 import React from 'react';
 
-const ASCIIHeader = () => {
-  // Your generated ASCII art
-  const demosLogoText = [
-    "XMMMMMMMMKo,    XMMMMMMMMMMM0   XMM      MMc    .dMMMMMMMb.      dMMMMMMMM0. ",
-    "XMM;     XMW    XMMl            XMMM    MMMc   .0MM     MM0;.   KMM         ",
-    "xMM;      MMc   XMMl            XMM WMMM MMc   o0M       MM0o   KMM         ",
-    "XMM;      MMC   XMMMMMMMMM0     XMM  WW  MMc   W0Ml      .M0W    dMMMMMMMb.  ",
-    "XMM;      MMc   XMMl            XMM      MMc   o0M;      ;M0o            MM0",
-    "XMM;     oMd    XMMl            XMM      MMc   .0MM      MM0.            MM0",
-    "XMMMMMMMMKP'    XMMMMMMMMMMM0   XMM      MMc    ';MMMMMMM:'     .0MMMMMMMm' ",
-  ];
+const demosLogoText = [
+  "XMMMMMMMKo,     XMMMMMMMMMMM0   XMM      MMc    .dMMMMMMMb.      dMMMMMMMM0. ",
+  "XMM;     XMW    XMMl            XMMM    MMMc   .0MM     MM0;.   KMM         ",
+  "xMM;      MMc   XMMl            XMM WMMM MMc   o0M       MM0o   KMM         ",
+  "XMM;      MMC   XMMMMMMMMM0     XMM  WW  MMc   W0Ml      .M0W    dMMMMMMMb. ",
+  "XMM;      MMc   XMMl            XMM      MMc   o0M;      ;M0o            MM0",
+  "XMM;     oMd    XMMl            XMM      MMc   .0MM      MM0.            MM0",
+  "XMMMMMMMMKP'    XMMMMMMMMMMM0   XMM      MMc    ';MMMMMMM:'     .0MMMMMMMm' ",
+];
 
+export default function ASCIIHeader(){
   return (
-    <div className="ascii-header">
-      <pre className="ascii-art">
-        {demosLogoText.join('\n')}
-      </pre>
-    </div>
+    <pre className="ascii-art" aria-label="DEMOS logo">
+      {demosLogoText.join('\n')}
+    </pre>
   );
-};
-
-export default ASCIIHeader;
+}
