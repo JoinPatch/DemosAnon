cc490903473d3a6647a47d6664884e3158371bbc
timf34 <farrelti@tcd.ie>
2025-08-03 19:37:43 +0100

    added code link

--- src/components/ASCIIHeader.jsx
+++ src/components/ASCIIHeader.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 console.log("made by tim: https://timfarrelly.com");
+console.log("code: https://github.com/timf34/DemosAnon");
 
 // Configuration flag - set to false to hide the grid
 const SHOW_GRID = false;
