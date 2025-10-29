fb6adf80994a5e1b71315294051efff791a191cf
timf34 <66926418+timf34@users.noreply.github.com>
2025-10-29 20:14:06 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  base: '/DemosAnon/',
   integrations: [react()],
   vite: {
     assetsInclude: ['**/*.md']
